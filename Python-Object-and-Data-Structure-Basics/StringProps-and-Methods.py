28f1eeaa1c5aa5291800a92a7ad652b3e3086d51
Elijah <elijahprince73@gmail.com>
2021-12-16 23:01:06 -0800

    string methods

---
+++ Python-Object-and-Data-Structure-Basics/StringProps-and-Methods.py
@@ -0,0 +1,22 @@
+name = 'sam'
+
+last_letters = name[1:]
+
+# string concatenation
+'p' + last_letters
+
+print('p' + last_letters)
+# returns pam
+
+2 + 3
+# returns 5
+
+'2' + '3'
+# returns '23'
+
+x = 'hello world'
+print(x.upper())
+# returns 'HELLO WORLD'
+
+print(x.split())
+# returns ['hello', 'world']
